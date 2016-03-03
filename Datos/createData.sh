41da85720d51941a93cbd68fe578aa6bbf48a74e
jorgejcabrera <cabrerajjorge@gmail.com>
2016-03-02 21:14:18 -0300

    agrego todos los datos de compra y venta del dolar a un unico archivo

--- Datos/createData.sh
+++ Datos/createData.sh
@@ -2,5 +2,5 @@
 
 for fileName in `ls | sed 's/createData.sh//'`; do
 	echo "parseando $fileName"
-	sed -i -e "s/\t/ /g" -i -e "s/\r//g" -i -e "s/\([a-zA-Z]*\)//gw datos.txt" $fileName 
+	sed -e 's/\t/ /g' -e 's/\r/ /g' -e 's/\([a-zA-Z]*\)//g' $fileName >> "datos.txt"
 done; 
